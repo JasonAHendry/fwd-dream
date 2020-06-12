72c2079135f9053ea437241f481204cc70d9c905
JasonAHendry <jason.ahendry@gmail.com>
2020-06-12 10:44:12 +0100

    Improved trajectory plots.

--- lib/plotting.py
+++ lib/plotting.py
@@ -103,230 +103,307 @@ def regress_violinplot(metric, x_h, df, ax, palette):
     return None
 
 
-days_per_year = 365
-years_per_major_tick = 10
-def format_xaxis(value, tick_number,
-                 days_per_year=days_per_year,
-                 years_per_major_tick=years_per_major_tick):
-    """
-    Plot as Decade Number as opposed to Day Number
-    """
-    return int(round(value / (days_per_year * years_per_major_tick)))
+# ================================================================================ #
+# Trajectory plots; longitudinal analysis of prevalence and genetic diversity
+# statistics
+# ================================================================================ #
 
 
-def plot_individual_timecourse(op, og, epoch_df,
-                               ax,
-                               genetic_alpha=0.8,
-                               genetic_names=None,
-                               genetic_metrics=None,
-                               focus_metric=None,
+def prevalence_trajectory_plot(ot, epoch_df, ax,
+                               norm_t0=None,
+                               indicate_epochs=None,
+                               indicate_equilibriums=None,
                                time_limits=None,
-                               output_path=None):
+                               years_per_major_tick=5):
     """
-    Plot the prevalence and, optionally, genetic diversity
-    statistics of a single forward-dream simulation through
-    time
+    Plot the trajectory of a genetic diversity
+    statistic through time
+    
+    
     """
     
-    # Host
-    ax.plot(op["t0"], op["HX"], color="blue", linewidth=0.8, label="Host All")
-    ax.plot(op["t0"], op["HmX"], color="darkblue", linewidth=0.8, label="Host Mixed")
-    
-    # Vector
-    ax.plot(op["t0"], op["VX"], color="red", linewidth=0.8, label="Vector All")
-    ax.plot(op["t0"], op["VmX"], color="darkred", linewidth=0.8, label="Vector Mixed")
-    
-    # Delineate Epochs and Equilibriums
-    for i, row in epoch_df.iterrows():
-        ax.vlines(ymin=0, ymax=1, x=row['t0'], color="darkgrey")
-        ax.hlines(xmin=row['t0'], xmax=row['t1'], y=row['x_h'], color="blue", linestyle="--")
-        ax.hlines(xmin=row['t0'], xmax=row['t1'], y=row['x_v'], color="red", linestyle="--")
-    
-    # Limits
-    ax.set_ylim([0, 1])
-    if time_limits is not None:
-        ax.set_xlim(time_limits)
+    # Normalize time to desired epoch
+    if norm_t0 is not None:
+        start = epoch_df.loc[norm_t0]
     else:
-        epoch_t0 = epoch_df.iloc[0].t0
-        epoch_t1 = epoch_df.iloc[-1].t1
-        time_limits = [epoch_t0, epoch_t1]
-        ax.set_xlim(time_limits)
-        
-    # Ticks
-    days_per_year = 365
-    years_per_major_tick = 10
-    ax.tick_params(axis='both', which='major', direction='in', length=10, labelsize=16)
-    ax.tick_params(axis='both', which='minor', direction='in', length=8, labelsize=16)
-    ax.xaxis.set_major_locator(plt.MultipleLocator(days_per_year * years_per_major_tick))
-    ax.xaxis.set_minor_locator(plt.MultipleLocator(days_per_year))
-    ax.xaxis.set_major_formatter(plt.FuncFormatter(format_xaxis))
-    
-    # Labels
-    ax.set_xlabel("Time (decades)", fontsize=18)
-    ax.set_ylabel("Prevalence", fontsize=18)
-    
-    # Legend
-    leg = ax.legend(ncol=2, fontsize=16)
-    for l in leg.legendHandles:
-        l.set_linewidth(3.0)
-        
-        
-    # Plot Genetic Diversity Lines
-    if genetic_metrics is not None:
-        for metric in genetic_metrics:
-            if metric is focus_metric:
-                axm = ax.twinx()
-                axm.plot(og["t0"], og[metric], "orange")
-                axm.set_ylabel(genetic_names[metric], fontsize=18)
-                axm.tick_params(axis='y', which='major', direction='in', length=10, labelsize=16)
-                axm.set_xlim(time_limits)
-            else:
-                axm = ax.twinx()
-                axm.plot(og["t0"], og[metric], "forestgreen", alpha=genetic_alpha)
-                axm.axes.get_yaxis().set_visible(False)
-                axm.set_xlim(time_limits)
-    
-    return None
-
-def plot_crash_response(metric, ot, epoch_df, d, e, ax):
-    """
-    Plot the detection and equilibrium times for a given metric
-    for a given simulation
-    """
-    # Normalize time to start of crash
-    start = epoch_df.loc["Crash", "t0"]
+        start = 0
     t0 = ot["t0"] - start
     
-    # Plot prevalence
-    ax.fill_between(x=t0, y1=0, y2=ot["HX"], 
-                    color="lightgrey", linewidth=0.8, 
-                    label="Host All")
-    ax.fill_between(x=t0, y1=0, y2=ot["VX"], 
-                    color="darkgrey", linewidth=0.8, 
-                    label="Vector All")
     
-    # Delineate key epochs
-    ax.axvline(epoch_df.loc["Crash", "t0"] - start, 
-           color="grey", alpha=0.75,
-           linewidth=3,
-           zorder=2)
-    ax.axvline(epoch_df.loc["CrashVar", "t0"] - start, 
-               color="grey", alpha=0.75,
-               linewidth=3,
-               zorder=2)
-    ax.axvline(epoch_df.loc["Recovery", "t0"] - start, 
-               color="grey", alpha=0.75,
-               linewidth=3,
-               zorder=2)
+    # Host Prevalence
+    ax.plot(t0, ot["HX"], color="blue", linewidth=0.8, label="Host All")
+    ax.plot(t0, ot["HmX"], color="darkblue", linewidth=0.8, label="Host Mixed")
+
+    # Vector Prevalence
+    ax.plot(t0, ot["VX"], color="red", linewidth=0.8, label="Vector All")
+    ax.plot(t0, ot["VmX"], color="darkred", linewidth=0.8, label="Vector Mixed")
     
-    # Labels
-    ax.set_ylabel("Prevalence")
-    ax.set_xlabel("Time (years)")
+        
+    # Set limits
+    ax.set_ylim((0, 1.0))
+    ax.set_xlim(time_limits - start)
     
-    # Limits
-    time_limits = (epoch_df.loc["InitVar", "t0"], epoch_df.loc["CrashVar", "t1"]) - start
-    ax.set_xlim(time_limits)
-    ax.set_ylim((0, 0.8))
-    
-    # Ticks, x-axis
+    # Set ticks, x-axis
     days_per_year = 365
-    years_per_major_tick = 10
+    ax.xaxis.set_major_locator(plt.MultipleLocator(days_per_year * years_per_major_tick))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(days_per_year))
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda v, i : int(v / days_per_year)))
+    
+    # Delineate epoch boundaries
+    if indicate_epochs is not None:
+        for epoch in indicate_epochs:
+            ax.axvline(epoch_df.loc[epoch] - start, 
+                       color="grey", alpha=0.75,
+                       linewidth=3, zorder=0)
+            
+    # Indicate equilibriums
+    if indicate_equilibriums is not None:
+        for epoch in indicate_equilibriums:
+            x = [epoch_df.loc[(epoch, "t0")] - start, epoch_df.loc[(epoch, "t1")] - start]
+            h = [epoch_df.loc[(epoch, "x_h")], epoch_df.loc[(epoch, "x_h")]]
+            v = [epoch_df.loc[(epoch, "x_v")], epoch_df.loc[(epoch, "x_v")]]
+            print(x, h)
+            ax.plot(x, h, linestyle="dashed", color="blue")
+            ax.plot(x, v, linestyle="dashed", color="red")
+            
+    return None
+
+
+
+
+def prevalence_trajectory_average_plot(ot_mu, ot_se, epoch_df, ax,
+                                       norm_t0=None,
+                                       indicate_epochs=None,
+                                       indicate_equilibriums=None,
+                                       time_limits=None,
+                                       years_per_major_tick=5):
+    """
+    Plot the trajectory of a genetic diversity
+    statistic through time
+    
+    
+    """
+    
+    # Normalize time to desired epoch
+    if norm_t0 is not None:
+        start = epoch_df.loc[norm_t0]
+    else:
+        start = 0
+    t0 = ot_mu["t0"] - start
+    
+    
+    # Plot prevalence
+    n_se = 1.96
+    se_alpha = 0.25
+    metrics = ["HX", "VX", "HmX", "VmX"]
+    cols = ["blue", "red", "darkblue", "darkred"]
+    for metric, col in zip(metrics, cols):
+        ax.plot(t0, ot_mu[metric], color=col, linewidth=0.8)
+        ax.fill_between(x=t0,
+                        y1=ot_mu[metric] - ot_se[metric] * n_se,
+                        y2=ot_mu[metric] + ot_se[metric] * n_se,
+                        color=col, alpha=se_alpha)
+    
+    # Set limits
+    ax.set_ylim((0, 1.0))
+    ax.set_xlim(time_limits - start)
+    
+    # Set ticks, x-axis
+    days_per_year = 365
+    ax.xaxis.set_major_locator(plt.MultipleLocator(days_per_year * years_per_major_tick))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(days_per_year))
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda v, i : int(v / days_per_year)))
+    
+    # Delineate epoch boundaries
+    if indicate_epochs is not None:
+        for epoch in indicate_epochs:
+            ax.axvline(epoch_df.loc[epoch] - start, 
+                       color="grey", alpha=0.75,
+                       linewidth=3, zorder=0)
+            
+    # Indicate equilibriums
+    if indicate_equilibriums is not None:
+        for epoch in indicate_equilibriums:
+            x = [epoch_df.loc[(epoch, "t0")] - start, epoch_df.loc[(epoch, "t1")] - start]
+            h = [epoch_df.loc[(epoch, "x_h")], epoch_df.loc[(epoch, "x_h")]]
+            v = [epoch_df.loc[(epoch, "x_v")], epoch_df.loc[(epoch, "x_v")]]
+            print(x, h)
+            ax.plot(x, h, linestyle="dashed", color="blue")
+            ax.plot(x, v, linestyle="dashed", color="red")
+            
+    return None
+
+
+
+
+def genetic_trajectory_plot(metric, ot, epoch_df,
+                            color, ax,
+                            norm_t0=None,
+                            indicate_epochs=None,
+                            time_limits=None,
+                            t_detection=None,
+                            t_equilibrium=None,
+                            years_per_major_tick=5):
+    """
+    Plot the trajectory of a genetic diversity
+    statistic through time
+    
+    
+    """
+    
+    # Normalize time to desired epoch
+    if norm_t0 is not None:
+        start = epoch_df.loc[norm_t0]
+    else:
+        start = 0
+    t0 = ot["t0"] - start
+    
+    
+    # Plot genetic metric
+    ax.plot(t0, ot[metric], 
+            color=color, linewidth=0.75, 
+            alpha=0.75, zorder=1)
+    
+    
+    # Demarcate detection & equilibrium
+    if t_detection is not None:
+        ax.axvline(t_detection[metric], 
+                   color=color, alpha=0.75,
+                   linewidth=3, linestyle="dashed",
+                   zorder=2)
+        
+    if t_equilibrium is not None:
+        ax.axvline(t_equilibrium[metric], 
+                   color=color, alpha=0.75,
+                   linewidth=3, linestyle="solid",
+                   zorder=2)
+        
+    # Set limits, x-axis
+    ax.set_xlim(time_limits - start)
+    
+    # Set ticks, x-axis
+    days_per_year = 365
     ax.xaxis.set_major_locator(plt.MultipleLocator(days_per_year * years_per_major_tick))
     ax.xaxis.set_minor_locator(plt.MultipleLocator(days_per_year))
     ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda v, i : int(v / days_per_year)))
     
     # Twin axis
+    ax.patch.set_visible(False)
     axm = ax.twinx()
-
-    # Plot Genetics
-    axm.plot(t0, ot[metric], color="steelblue", linewidth=0.75, alpha=0.75)
+    axm.set_zorder(ax.get_zorder() - 1)
     
-    # Labels
-    axm.set_ylabel(genetic_names[metric])
+    # Plot prevalence
+    axm.fill_between(x=t0, y1=0, y2=ot["HX"], 
+                    color="lightgrey", linewidth=0.8, 
+                    label="Host All")
+    axm.fill_between(x=t0, y1=0, y2=ot["VX"], 
+                    color="darkgrey", linewidth=0.8, 
+                    label="Vector All")
+    
+    # Delineate epoch boundaries
+    if indicate_epochs is not None:
+        for epoch in indicate_epochs:
+            axm.axvline(epoch_df.loc[epoch] - start, 
+                        color="grey", alpha=0.75,
+                        linewidth=3,
+                        zorder=0)
+    
+    
+    # Hide y-labels
+    axm.set_yticklabels("")
     
     # Limits
-    axm.set_xlim(time_limits)
-
-    # Demarcate detection
-    axm.axvline(d[metric], 
-                color="orange", alpha=0.75,
-                linewidth=3,
-                zorder=2)
-
-    axm.axvline(e[metric], 
-                color="red", alpha=0.75,
-                linewidth=3,
-                zorder=2)
+    axm.set_xlim(time_limits - start)
+    axm.set_ylim((0, 0.8))
     
     return None
 
 
 
-def plot_recovery_response(metric, ot, epoch_df, d, e, ax):
+
+def genetic_trajectory_average_plot(metric, ot_mu, ot_se, epoch_df,
+                                    color, ax,
+                                    norm_t0=None,
+                                    indicate_epochs=None,
+                                    time_limits=None,
+                                    t_detection=None,
+                                    t_equilibrium=None,
+                                    years_per_major_tick=5):
     """
-    Plot the detection and equilibrium times for a given metric
-    for a given simulation
+    Plot the trajectory of a genetic diversity
+    statistic through time
+    
+    
     """
-    # Normalize time to start of crash
-    start = epoch_df.loc["Recovery", "t0"]
-    t0 = ot["t0"] - start
     
-    # Plot prevalence
-    ax.fill_between(x=t0, y1=0, y2=ot["HX"], 
-                    color="lightgrey", linewidth=0.8, 
-                    label="Host All")
-    ax.fill_between(x=t0, y1=0, y2=ot["VX"], 
-                    color="darkgrey", linewidth=0.8, 
-                    label="Vector All")
+    # Normalize time to desired epoch
+    if norm_t0 is not None:
+        start = epoch_df.loc[norm_t0]
+    else:
+        start = 0
+    t0 = ot_mu["t0"] - start
     
-    # Delineate key epochs
-    ax.axvline(epoch_df.loc["CrashVar", "t0"] - start, 
-           color="grey", alpha=0.75,
-           linewidth=3,
-           zorder=2)
-    ax.axvline(epoch_df.loc["Recovery", "t0"] - start, 
-               color="grey", alpha=0.75,
-               linewidth=3,
-               zorder=2)
     
-    # Labels
-    ax.set_ylabel("Prevalence")
-    ax.set_xlabel("Time (years)")
+    # Plot genetic metric
+    n_se = 1.96
+    se_alpha = 0.25
+    ax.plot(t0, ot_mu[metric], color=color, linewidth=1.0)
+    ax.fill_between(x=t0,
+                    y1=ot_mu[metric] - ot_se[metric] * n_se,
+                    y2=ot_mu[metric] + ot_se[metric] * n_se,
+                    color=color, alpha=se_alpha)
     
-    # Limits
-    time_limits = (epoch_df.loc["CrashVar", "t0"], epoch_df.loc["Recovery", "t1"]) - start
-    ax.set_xlim(time_limits)
-    ax.set_ylim((0, 0.8))
     
-    # Ticks, x-axis
+    # Demarcate detection & equilibrium
+    if t_detection is not None:
+        ax.axvline(t_detection[metric], 
+                   color=color, alpha=0.75,
+                   linewidth=3, linestyle="dashed",
+                   zorder=2)
+        
+    if t_equilibrium is not None:
+        ax.axvline(t_equilibrium[metric], 
+                   color=color, alpha=0.75,
+                   linewidth=3, linestyle="solid",
+                   zorder=2)
+        
+    # Set limits, x-axis
+    ax.set_xlim(time_limits - start)
+    
+    # Set ticks, x-axis
     days_per_year = 365
-    years_per_major_tick = 10
     ax.xaxis.set_major_locator(plt.MultipleLocator(days_per_year * years_per_major_tick))
     ax.xaxis.set_minor_locator(plt.MultipleLocator(days_per_year))
     ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda v, i : int(v / days_per_year)))
     
     # Twin axis
+    ax.patch.set_visible(False)
     axm = ax.twinx()
-
-    # Plot Genetics
-    axm.plot(t0, ot[metric], color="steelblue", linewidth=0.75, alpha=0.75)
+    axm.set_zorder(ax.get_zorder() - 1)
     
-    # Labels
-    axm.set_ylabel(genetic_names[metric])
+    # Plot prevalence
+    axm.fill_between(x=t0, y1=0, y2=ot_mu["HX"], 
+                    color="lightgrey", linewidth=0.8, 
+                    label="Host All")
+    axm.fill_between(x=t0, y1=0, y2=ot_mu["VX"], 
+                    color="darkgrey", linewidth=0.8, 
+                    label="Vector All")
+    
+    # Delineate epoch boundaries
+    if indicate_epochs is not None:
+        for epoch in indicate_epochs:
+            axm.axvline(epoch_df.loc[epoch] - start, 
+                        color="grey", alpha=0.75,
+                        linewidth=3,
+                        zorder=0)
+    
+    
+    # Hide y-labels
+    axm.set_yticklabels("")
     
     # Limits
-    axm.set_xlim(time_limits)
-
-    # Demarcate detection
-    axm.axvline(d[metric], 
-                color="orange", alpha=0.75,
-                linewidth=3,
-                zorder=2)
-
-    axm.axvline(e[metric], 
-                color="red", alpha=0.75,
-                linewidth=3,
-                zorder=2)
+    axm.set_xlim(time_limits - start)
+    axm.set_ylim((0, 1.0))
     
     return None
 
